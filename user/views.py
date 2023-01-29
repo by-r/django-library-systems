a07f9509e2e1fbfaaf10f03a1e576d082bf28ec3
Mohd Razzeq <rzq272@gmail.com>
2023-01-29 19:20:54 +0800

    Fixed auth access

--- user/views.py
+++ user/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views.generic import ListView
 from catalog.models import BookInstance
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 # Create your views here.
 
 
-class UserListView(ListView):
+class UserListView(PermissionRequiredMixin, ListView):
+    permission_required = 'login'
     model = User
     template_name = "user/profile.html"
     paginate_by = 5
